36c5411f6cc0d493e653a39d109a71b532f511e4
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-03-19 13:57:25 -0400

    .

--- Swerve reborn/src/main/java/frc/robot/RobotContainer.java
+++ Swerve reborn/src/main/java/frc/robot/RobotContainer.java
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
@@ -67,13 +71,10 @@ public class RobotContainer {
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-        // Add button binding to start the FollowAprilTagCommand
-        joystick.x().whileTrue(new FollowAprilTagCommand(visionSubsystem, drivetrain));
-
         drivetrain.registerTelemetry(logger::telemeterize);
     }
 
     public Command getAutonomousCommand() {
-        return new PathPlannerAuto("Center Auto");
+        return Commands.print("No autonomous command configured");
     }
 }
