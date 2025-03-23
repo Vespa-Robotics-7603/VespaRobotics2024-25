f25056df68f53d9cf1ba08f37bae0ace62cdb94f
notaoeu <notaoeu@tutanota.com>
2025-03-22 23:03:25 -0400

    Properly fix TrajectoryFollower

--- trajectory-testing/src/main/java/frc/robot/subsystems/TrajectoryFollower.java
+++ trajectory-testing/src/main/java/frc/robot/subsystems/TrajectoryFollower.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import java.util.List;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
@@ -9,6 +8,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
@@ -16,86 +16,80 @@ import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.SwerveUtils.TrajectoryTarget2d;
 
-import static java.lang.Math.PI;
-
+// TODO: Document and test everything, refactor if necessary.
+// TODO: Javadoc everything once code implementation is finalized
 public class TrajectoryFollower {
+    private final double MAX_SPEED = 10;
+    private final double MAX_ROTATIONAL_SPEED = 4;
+    private final double MAX_ROTATIONAL_ACCELERATION = 3.14;
     CommandSwerveDrivetrain drivetrain;
     SwerveDriveKinematicsConstraint constraint;
     HolonomicDriveController controller;
-    
+
+    // TODO: Determine our robot's ideal max speed.
     public TrajectoryFollower(CommandSwerveDrivetrain drivetrain){
         this.drivetrain = drivetrain;
-        constraint = 
-        new SwerveDriveKinematicsConstraint(drivetrain.getKinematics(), maxSpeed);
+        constraint = new SwerveDriveKinematicsConstraint(
+            drivetrain.getKinematics(),
+            MAX_SPEED
+        );
         // Create a voltage constraint to ensure we don't accelerate too fast
         controller = new HolonomicDriveController(
             new PIDController(0.11, 0, 0), 
             new PIDController(0.18, 0, 0),
             new ProfiledPIDController(11, 0, 0,
-                new TrapezoidProfile.Constraints(maxSpeed, 3.14)
+                new TrapezoidProfile.Constraints(MAX_ROTATIONAL_SPEED, MAX_ROTATIONAL_ACCELERATION)
             )
         );
     }
-    
-    double maxSpeed = 3;
-    
-    public Command FollowCommand(){
 
+    // TODO: Reformat this code so that it's more readable.
+    public TrajectoryConfig generateTrajectoryConfig(double speed, double acceleration) {
         // Create config for trajectory
-        TrajectoryConfig config =
-            new TrajectoryConfig(
-                maxSpeed,
-                3.14
+        return new TrajectoryConfig(
+                speed,
+                acceleration
             )
             // Add kinematics to ensure max speed is actually obeyed
             .setKinematics(drivetrain.getKinematics())
             // Apply the voltage constraint
             .addConstraint(constraint);
-                
+    }
 
-        // An example trajectory to follow. All units in meters.
-        Trajectory exampleTrajectory =
-            TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(/* new Translation2d(1, 1), new Translation2d(2, -1) */),
-                // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(0, 1, new Rotation2d(PI/2)),
-                // Pass config
-                config);
-        // Trajectory f= 
-
-        SwerveControllerCommand command = new SwerveControllerCommand(
-            exampleTrajectory, 
+    public Trajectory generateTrajectory(double speed, double acceleration, TrajectoryTarget2d target) {
+        TrajectoryConfig config = generateTrajectoryConfig(speed, acceleration);
+        return TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            target.getWaypoints(),
+            new Pose2d(target.getTranslation(), target.getRotation()),
+            config
+        );
+    }
+    
+    public SwerveControllerCommand generateMovementCommand(Trajectory trajectory) {
+        return new SwerveControllerCommand(
+            trajectory,
             drivetrain::getPose, 
             drivetrain.getKinematics(), 
-            controller, 
+            controller,
             this::moveWithState, 
             drivetrain
         );
+    }
 
-        // Reset odometry to the initial pose of the trajectory, run path following
-        // command, then stop at the end.
-        return Commands.runOnce(() ->{
-            //reset odometry?
-            // drivetrain.tareEverything();
-            System.out.println("Reset odo here");
-        } )
-            .andThen(command)
-            .andThen(Commands.runOnce(() -> {
-                //brake
-                System.out.println("BRAKING");
-                drivetrain.setControl(new SwerveRequest.SwerveDriveBrake());
-            })
-        );
+    public Command moveToTarget(double speed, double acceleration, TrajectoryTarget2d target){
+        Trajectory trajectory = generateTrajectory(speed, acceleration, target);
+        SwerveControllerCommand command = generateMovementCommand(trajectory);
+        return command;
     }
     
     private void moveWithState(SwerveModuleState... states){
         //TODO
+        // Editor's note: next time, please be more specific with your TODOs...
         System.out.println("Applying speeds...");
         drivetrain.setControl(
             new SwerveRequest.ApplyRobotSpeeds().withSpeeds(
@@ -105,7 +99,6 @@ public class TrajectoryFollower {
     }
     
     public Command followTraj(Trajectory trajectoryToFollow){
-        
         return new SwerveControllerCommand(
             trajectoryToFollow, 
             drivetrain::getPose, 
