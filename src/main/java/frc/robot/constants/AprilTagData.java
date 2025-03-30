6d165dc1dc13b155c20ab37542c700e08a5f245e
notaoeu <notaoeu@tutanota.com>
2025-03-29 23:04:52 -0400

    Finish pushing april tag alignment fixes

---
+++ src/main/java/frc/robot/constants/AprilTagData.java
@@ -0,0 +1,17 @@
+package frc.robot.constants;
+
+public class AprilTagData {
+    public static final int[] INTAKE_TAGS = {
+        // Red intake tag IDs
+        1, 2,
+        // Blue intake tag IDs
+        12, 13
+    };
+    
+    public static final int[] REEF_TAGS = {
+        // Red reef tag IDs
+        6, 7, 8, 9, 10, 11,
+        // Blue reef tag IDs
+        17, 18, 19, 20, 21, 22
+    };
+}
